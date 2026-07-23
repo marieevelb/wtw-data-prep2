ef2701a630127b729cc22abba5c5b69cb7e74df2
marieevelb <Marie-Eve.Le.Ber@USherbrooke.ca>
2026-07-23 15:48:41 -0400

    Update of 01a Extract Vector to Polygon
    The 01a Extract Vector to Polygon tool was updated to allow batch processing without the need for the data prep csv.

--- wtw-utils/extract_vector_to_polygon.py
+++ wtw-utils/extract_vector_to_polygon.py
@@ -13,31 +13,8 @@ arcpy.env.overwriteOutput = True
 
 # Get user params
 input_poly = arcpy.GetParameterAsText(0)
-input_vect = arcpy.GetParameterAsText(1)
-input_unit = arcpy.GetParameterAsText(2)
-input_colname = arcpy.GetParameterAsText(3)
-input_csv = arcpy.GetParameterAsText(4)
+input_vect = arcpy.GetParameterValue(4)
 
-# Set emtpy lists to populate
-vector_lst = []
-short_name_lst = []
-unit_lst = []
-
-# Shape csv input paramters    
-if input_csv:
-  csv_file_name = os.path.basename(input_csv)
-  arcpy.AddMessage(f"{csv_file_name} provided, using batch inputs.")
-  batch_df = pd.read_csv(input_csv)
-  batch_df = batch_df[batch_df["datatype"] == "vector"] # needed to filter
-  vector_lst.extend(batch_df['conversion_ready_input'].tolist())
-  unit_lst.extend(batch_df['unit'].tolist())
-  short_name_lst.extend(batch_df['short_name'].tolist())
-else:
-  arcpy.AddMessage("No dataprep.csv provided, using single vector input.")
-  # If no csv provided, use single vector input
-  vector_lst.append(input_vect)
-  short_name_lst.append(input_colname)
-  unit_lst.append(input_unit)
 
 # Create wtw id
 arcpy.AddField_management(input_poly, "WTWID", "LONG")
@@ -47,9 +24,9 @@ with arcpy.da.UpdateCursor(input_poly, ["WTWID"]) as cursor:
       cursor.updateRow(row)
 
 # Process each list item
-l = len(vector_lst)
+l = len(input_vect)
 counter = 1
-for vector, short_name, unit in zip(vector_lst, short_name_lst, unit_lst):
+for vector, short_name, unit in (input_vect):
   file_name = arcpy.Describe(vector).name
   arcpy.AddMessage(f"... Processing {counter} of {l}: {file_name}")
 
@@ -62,4 +39,3 @@ for vector, short_name, unit in zip(vector_lst, short_name_lst, unit_lst):
   )
   ## advance counter
   counter += 1
-  
